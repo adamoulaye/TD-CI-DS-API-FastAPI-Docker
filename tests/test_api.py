84e72700f69443a6cd57371624b206474e5deea8
adamoulaye <adamlaye06@gmail.com>
2026-09-03 23:30:05 -0400

    Ajout tests exercice 1 + pipeline CI bonifié

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,41 +1,55 @@
+# tests/test_api.py
 import pytest
-from httpx import ASGITransport, AsyncClient
-
+from httpx import AsyncClient
 from app.main import app
-
 
 @pytest.mark.anyio
 async def test_predict_success():
-    transport = ASGITransport(app=app)
-
-    async with AsyncClient(
-        transport=transport,
-        base_url="http://test",
-    ) as client:
-        response = await client.post(
-            "/predict",
-            json={"features": [3.5, 1.2, 4.9]},
-        )
-
-    assert response.status_code == 200
-    assert response.json() == {"predictions": [7.0, 2.4, 9.8]}
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+            "features": [3.5, 1.2, 4.9]
+        })
+    assert resp.status_code == 200
+    assert {"predictions": [7.0, 2.4, 9.8]} == resp.json()
 
 
 @pytest.mark.anyio
 async def test_predict_unprocessable_entity():
-    transport = ASGITransport(app=app)
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+            "feature1": 3.5,
+            "feature2": 1.2,
+            "feature3": 4.9
+        })
+    assert resp.status_code == 422
 
-    async with AsyncClient(
-        transport=transport,
-        base_url="http://test",
-    ) as client:
-        response = await client.post(
-            "/predict",
-            json={
-                "feature1": 3.5,
-                "feature2": 1.2,
-                "feature3": 4.9,
-            },
-        )
 
-    assert response.status_code == 422
+# --- Exercice 1 ---
+
+@pytest.mark.anyio
+async def test_predict_correct_values():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+            "features": [1.0, 2.0, 3.0]
+        })
+    assert resp.status_code == 200
+    assert {"predictions": [2.0, 4.0, 6.0]} == resp.json()
+
+
+@pytest.mark.anyio
+async def test_predict_incorrect_values():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+            "features": [1.0, 2.0, 3.0]
+        })
+    assert resp.status_code == 200
+    assert {"predictions": [3.0, 5.0, 7.0]} != resp.json()
+
+
+@pytest.mark.anyio
+async def test_predict_missing_features_field():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+            "wrong_field": [3.5, 1.2, 4.9]
+        })
+    assert resp.status_code == 422 
